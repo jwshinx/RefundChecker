b9b58ae69b4be03a450fdbfdee72de13f3e328e4
Joel Shin <jwshin@gmail.com>
2013-02-11 16:55:32 -0800

    cleanedup

--- lib/payment_manager.rb
+++ lib/payment_manager.rb
@@ -1,9 +1,10 @@
 class PaymentManager
+
  attr_reader :cim_count, :settlement_count, :cim_order, :settlement_order
 
  def initialize params
-  create_valid_count_check_methods_of :cim, params
-  create_valid_count_check_methods_of :settlement, params
+  valid_count_check_methods_of :cim, params
+  valid_count_check_methods_of :settlement, params
   check_valid_attributes params
   params.has_key?(:cim_count) ? @cim_count = params[:cim_count] : @cim_count = 0
   params.has_key?(:settlement_count) ? @settlement_count = params[:settlement_count] : @settlement_count = 0
@@ -11,7 +12,7 @@ class PaymentManager
   (params.has_key?(:settlement_order) && params[:settlement_order] == :asc) ? @settlement_order = :asc : @settlement_order = :desc 
  end
 
- def create_valid_count_check_methods_of attribute, arg
+ def valid_count_check_methods_of attribute, arg
   self.class.send(:define_method, "#{attribute}_count_is_undefined_in") do |options|
    !options.has_key?("#{attribute}_count".to_sym)
   end
