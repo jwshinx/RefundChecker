809d85f24bab4ec5533cdb2781c0ceadec4ed8b2
Joel Shin <jwshin@gmail.com>
2013-02-11 17:11:20 -0800

    refactrd

--- lib/payment_manager.rb
+++ lib/payment_manager.rb
@@ -6,10 +6,8 @@ class PaymentManager
   valid_count_check_methods_of :cim, params
   valid_count_check_methods_of :settlement, params
   check_valid_attributes params
-  params.has_key?(:cim_count) ? @cim_count = params[:cim_count] : @cim_count = 0
-  params.has_key?(:settlement_count) ? @settlement_count = params[:settlement_count] : @settlement_count = 0
-  (params.has_key?(:cim_order) && params[:cim_order] == :asc) ? @cim_order = :asc : @cim_order = :desc 
-  (params.has_key?(:settlement_order) && params[:settlement_order] == :asc) ? @settlement_order = :asc : @settlement_order = :desc 
+  set_count_defaults params 
+  set_order_defaults params 
  end
 
  def valid_count_check_methods_of attribute, arg
@@ -23,6 +21,14 @@ class PaymentManager
 
 private
 
+ def set_order_defaults options 
+  (options.has_key?(:cim_order) && options[:cim_order] == :asc) ? @cim_order = :asc : @cim_order = :desc 
+  (options.has_key?(:settlement_order) && options[:settlement_order] == :asc) ? @settlement_order = :asc : @settlement_order = :desc 
+ end
+ def set_count_defaults options
+  options.has_key?(:cim_count) ? @cim_count = options[:cim_count] : @cim_count = 0
+  options.has_key?(:settlement_count) ? @settlement_count = options[:settlement_count] : @settlement_count = 0
+ end
  def check_valid_attributes options
   raise 'Invalid cim count.' unless cim_count_is_undefined_in(options) || cim_count_is_number_in(options)
   raise 'Invalid settlement count.' unless settlement_count_is_undefined_in(options) || settlement_count_is_number_in(options)
